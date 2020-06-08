a17724f95a22442d6b51f13b985b520cf6a6244c
Nathan Atherton <nathanatherton2@gmail.com>
2020-06-08 19:30:49 +0100

    Created functions to work out correct final prices of products
    Todo: Move function into cart component to get the price updating 
    automatically

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
 const Cart = props => {
-  return(
+
+  let product = props;
+  return (
     <div>
-      <h2>Your Cart</h2>
+    <h2>Your Cart</h2>
+    <p>Your items</p>
+    {product.noA > 0 ? <div><p>Product A: {product.noA} cost: {product.A}</p></div> : ''}
+    {product.noB > 0 ? <div><p>Product B: {product.noB} cost: {product.B}</p></div> : ''}
+    {product.noC > 0 ? <div><p>Product C: {product.noC} cost: {product.C}</p></div> : ''}
+    {product.noD > 0 ? <div><p>Product D: {product.noD} cost: {product.D}</p></div> : ''}
+    <p>Total: {product.totalPrice}</p>
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -11,7 +11,81 @@ class Main extends React.Component {
       noOfB: 0,
       noOfC: 0,
       noOfD: 0,
+      totalPrice: 0,
+      aPrice: 0,
+      bPrice: 0,
+      cPrice: 0,
+      dPrice: 0,
+      aDealPrice: 0,
+      bDealPrice: 0,
+      finalAPrice: 0,
+      finalBPrice: 0,
+      finalCPrice: 0,
+      finalDPrice: 0,
     }
+  }
+  setProductPrices = () => {
+    let aPrice, bPrice, cPrice, dPrice;
+    let aDealPrice, bDealPrice;
+    let product = Object.values(products);
+
+    for (var i = 0; i < products.length; i++) {
+      {i === 0 ? aPrice = product[i].price :
+       i === 1 ? bPrice = product[i].price :
+       i === 2 ? cPrice = product[i].price :
+       i === 3 ? dPrice = product[i].price : console.log('no match');}
+    }
+    for (var i = 0; i < products.length; i++) {
+      if (product[i].deal) {
+        {i === 0 ? aDealPrice = product[i].dealPrice :
+         i === 1 ? bDealPrice = product[i].dealPrice : console.log('no match');}
+      }
+    }
+    this.setState({
+      aPrice: aPrice,
+      bPrice: bPrice,
+      cPrice: cPrice,
+      dPrice: dPrice,
+      aDealPrice: aDealPrice,
+      bDealPrice: bDealPrice,
+    })
+
+  }
+
+  updateTotalPrice = () => {
+    let a = this.state.noOfA,
+        b = this.state.noOfB,
+        c = this.state.noOfC,
+        d = this.state.noOfD;
+
+    let aDeals = Math.floor(a/3),
+        bDeals = Math.floor(b/2);
+
+    let finalAPrice,
+        finalBPrice,
+        finalCPrice = c * this.state.cPrice,
+        finalDPrice = d * this.state.dPrice;
+
+    if (Number(a) % 3 != 0) {
+      let aString = (a/3).toString().includes('.66')
+      {aString ? finalAPrice = (this.state.aDealPrice * aDeals) + (this.state.aPrice * 2)
+       : finalAPrice = (this.state.aDealPrice * aDeals) + this.state.aPrice}
+    } else {
+      finalAPrice = aDeals * this.state.aDealPrice;
+    }
+
+    if (Number(b) % 2 != 0) {
+      finalBPrice = (this.state.bDealPrice * bDeals) + this.state.bPrice;
+    } else {
+      finalBPrice = bDeals * this.state.bDealPrice;
+    }
+
+    this.setState({
+      finalAPrice: finalAPrice,
+      finalBPrice: finalBPrice,
+      finalCPrice: finalCPrice,
+      finalDPrice: finalDPrice
+    })
   }
 
   handleChange = event => {
@@ -20,14 +94,20 @@ class Main extends React.Component {
     this.setState({
       [name]: value,
     })
+    this.updateTotalPrice()
+  }
+
+  componentDidMount() {
+    this.setProductPrices();
   }
 
   render() {
-    const { noOfA, noOfB, noOfC, noOfD } = this.state;
+    const { noOfA, noOfB, noOfC, noOfD,
+            finalAPrice, finalBPrice, finalCPrice, finalDPrice } = this.state;
     return (
       <>
         <Products productsList={products} A={noOfA} B={noOfB} C={noOfC} D={noOfD} handleChange={this.handleChange}/>
-        <Cart productsList={products} A={noOfA} B={noOfB} C={noOfC} D={noOfD} handleChange={this.handleChange}/>
+        <Cart noA={noOfA} noB={noOfB} noC={noOfC} noD={noOfD} A={finalAPrice} B={finalBPrice} C={finalCPrice} D={finalDPrice}/>
       </>
     )
   }
